d89ecf17f9a172d8912c42be8111a15cfb710a2e
rachel4w2 <87238236+rachel4w2@users.noreply.github.com>
2023-04-09 23:27:35 +0900

    feature/202102644

--- src/main/java/com/cnu/real_coding_server/service/PostService.java
+++ src/main/java/com/cnu/real_coding_server/service/PostService.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 public class PostService {
 
     private final PostRepository postRepository;
+
     public Post createPost(PostRequest postRequest) {
         return postRepository.save(postRequest.toEntity());
     }
